ec17994ec1c2031f1f45936a29efa74c2e3ea5ad
uvais8958 <uvaisa740@gmail.com>
2026-06-10 14:33:33 +0530

    Fix HTTPS URL

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const inputBox=document.querySelector('.inputBox');
 const getMovieInfo= async(movie)=>{
 try{
     const myApiKey="5e52e973";
-  const url=`http://www.omdbapi.com/?apikey=${myApiKey}&t=${movie}`;
+  const url=`https://www.omdbapi.com/?apikey=${myApiKey}&t=${movie}`;
 
   const response= await fetch(url);
   if(!response.ok){
